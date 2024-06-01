4e3755cf70917ef65976938b8ea307ca9dbd6a13
Jamyle Elen <jamyleelen09@gmail.com>
2024-06-01 08:05:44 -0300

    listas

---
+++ Estudos/dicionarios/dicionario.py
@@ -0,0 +1,10 @@
+lista_npcs = [
+    {"name": "Monstro 1", "level": 1},
+    {"name": "Monstro 2", "level": 2},
+    {"name": "Monstro 3", "level": 3},
+    {"name": "Monstro 4", "level": 4},
+]
+
+for npc in lista_npcs:
+    # print(npc['name'], npc['level'])
+    print(f"Nome: {npc['name']} // Level: {npc['level']}" )
