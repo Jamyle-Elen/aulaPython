869b3e297909647bb7bb5caa28bd746305334a00
Jamyle Elen <jamyleelen09@gmail.com>
2024-06-01 11:25:57 -0300

    aula

--- Estudos/dicionarios/dicionario.py
+++ Estudos/dicionarios/dicionario.py
@@ -2,7 +2,7 @@ from random import randint
 
 lista_npcs = []
 
-def create_monster():
+def create_npc():
     level = randint(0, 50)
     new_npc = {
         "name": f"Monster #{level}",
@@ -11,8 +11,17 @@ def create_monster():
         "hp": 100 * level
     }
 
-    lista_npcs.append()
+    lista_npcs.append(new_npc)
 
-create_monster()
+def display_npc():
+    for npc in lista_npcs:
+        print(f"Name: {npc['name']} // Level: {npc['level']} // Damage: {npc['damage']} // HP: {npc['hp']}")
 
-print(lista_npcs)
+def generate_npcs(n_npcs):
+    for x in range(n_npcs):
+        print(x)
+        create_npc()
+
+
+
+generate_npcs(5)
