7adbbabd4bdfa0d15b8438d2fb97dbf9a011233d
Jamyle Elen <jamyleelen09@gmail.com>
2024-06-01 08:22:29 -0300

    create monster

--- Estudos/dicionarios/dicionario.py
+++ Estudos/dicionarios/dicionario.py
@@ -1,10 +1,18 @@
-lista_npcs = [
-    {"name": "Monstro 1", "level": 1},
-    {"name": "Monstro 2", "level": 2},
-    {"name": "Monstro 3", "level": 3},
-    {"name": "Monstro 4", "level": 4},
-]
+from random import randint
 
-for npc in lista_npcs:
-    # print(npc['name'], npc['level'])
-    print(f"Nome: {npc['name']} // Level: {npc['level']}" )
+lista_npcs = []
+
+def create_monster():
+    level = randint(0, 50)
+    new_npc = {
+        "name": f"Monster #{level}",
+        "level": level,
+        "damage": 5 * level,
+        "hp": 100 * level
+    }
+
+    lista_npcs.append()
+
+create_monster()
+
+print(lista_npcs)
